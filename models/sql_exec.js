9f20c16c9e595c19d0046f6ddda8f99ad82cac3d
Kevinnan-teen <Kevinnan813@gmail.com>
2022-02-28 15:13:36 +0800

    数据库CURD操作的封装

---
+++ models/sql_exec.js
@@ -0,0 +1,60 @@
+const sqlite3 = require('sqlite3');
+const path    = require('path');
+
+//打开/创建 数据库
+const database_path = 'production_data.db';
+const db_name = path.join(__dirname, database_path);
+
+const db = new sqlite3.Database(db_name, err => {
+    if(err)
+        console.log(err);
+    else
+        console.log('open database successfully!');
+});
+
+
+
+let insertData = function(sql_query, params){
+    return new Promise((resolve, reject) =>{
+        db.run(sql_query, params, err => {
+            if(err){
+                console.log(err);
+                reject(err);
+            }else{
+                console.log('insert successfully!');
+                resolve(true);
+            }
+        })
+    });
+}
+
+let findData = function(sql_query){
+    return new Promise((resolve, reject) => {
+        db.all(sql_query, (err, rows) => {
+            if(err){
+                console.log(err);
+                reject(err);
+            }else{
+                console.log("find successfully!");
+                resolve(rows);
+            }
+        })
+    })
+}
+
+let deleteData = function(sql_query){
+    return new Promise((resolve, reject) => {
+        db.run(sql_query, err => {
+            if(err){
+                console.log(err);
+                reject(err);
+            }else{
+                console.log('delete successfully!');
+                resolve(true);
+            }
+        })
+    })
+}
+
+
+module.exports = {insertData, findData, deleteData};
