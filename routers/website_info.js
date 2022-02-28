fbbe2b1db9944168c4ecc75bc454a6c56562553b
Kevinnan-teen <Kevinnan813@gmail.com>
2022-02-28 15:03:53 +0800

    网站信息管理页面路由

---
+++ routers/website_info.js
@@ -0,0 +1,15 @@
+const Router = require('koa-router');
+
+const {websiteInfoRender, websiteInfoGetAPI, modifyWebsiteInfo,
+       AddWebsiteInfo, websiteInfoPostAPI, deleteWebsiteInfoPostAPI} = require('../controllers/website_info');
+
+//网站信息和管理页面路由
+let website_info_router = new Router();
+website_info_router.get('website_info', websiteInfoRender)
+.get('api/website-info', websiteInfoGetAPI)
+.get('add_website_info', AddWebsiteInfo)
+.post('api/website-info-post', websiteInfoPostAPI)
+.get('modify_website_info', modifyWebsiteInfo)
+.post('api/website-info-delete-post', deleteWebsiteInfoPostAPI)
+
+module.exports = website_info_router
