60542cc14edf95571bc9359da554de0b74216dbb
Kevinnan-teen <Kevinnan813@gmail.com>
2022-02-28 18:23:58 +0800

    网站信息路由添加数据更新功能

--- routers/website_info.js
+++ routers/website_info.js
@@ -1,7 +1,7 @@
 const Router = require('koa-router');
 
-const {websiteInfoRender, websiteInfoGetAPI, modifyWebsiteInfo,
-       AddWebsiteInfo, websiteInfoPostAPI, deleteWebsiteInfoPostAPI} = require('../controllers/website_info');
+const {websiteInfoRender, websiteInfoGetAPI, modifyWebsiteInfo, AddWebsiteInfo, 
+       websiteInfoPostAPI, deleteWebsiteInfoPostAPI, modifyWebsiteInfoPostAPI} = require('../controllers/website_info');
 
 //网站信息和管理页面路由
 let website_info_router = new Router();
@@ -11,5 +11,6 @@ website_info_router.get('website_info', websiteInfoRender)
 .post('api/website-info-post', websiteInfoPostAPI)
 .get('modify_website_info', modifyWebsiteInfo)
 .post('api/website-info-delete-post', deleteWebsiteInfoPostAPI)
+.post('api/website-info-modify-post', modifyWebsiteInfoPostAPI)
 
 module.exports = website_info_router
